954b028d460e53d9c76fa906b8c7cc0ed044b933
Eric <5846501+ericphanson@users.noreply.github.com>
2019-09-04 19:18:04 +0100

    Fix link

--- deps/build.jl
+++ deps/build.jl
@@ -44,7 +44,7 @@ if dl_info === nothing && unsatisfied && !custom_library
             
             
             prefix = Prefix(joinpath(prefix, "bin"))
-            BinaryProvider.download_verify("https://github.com/ericphanson/SDPA_GMP_Builder/tree/master/deps/sdpa_gmp_wsl",  # url
+            BinaryProvider.download_verify("https://github.com/ericphanson/SDPA_GMP_Builder/raw/master/deps/sdpa_gmp_wsl",  # url
                     "f8ed0c3f2aefa1ab5a90f1999c78548625f6122f969972b8a51b54a0017b3a59", # hash
                     joinpath(prefix.path,"sdpa_gmp");  # destination
                     verbose=verbose)
