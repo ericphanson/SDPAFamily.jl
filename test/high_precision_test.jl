2908a2775373d371023fcc6043c99ef840521bb3
Jiazheng Zhu <zhujiazheng98@hotmail.com>
2019-09-06 15:41:09 +0100

    update tests

--- test/high_precision_test.jl
+++ test/high_precision_test.jl
@@ -7,11 +7,11 @@ using SDPA_GMP
     s1, s2 = [big"0.25" big"-0.25"*im; big"0.25"*im big"0.25"], [big"0.5" big"0.0"; big"0.0" big"0.0"]
     p = Problem{BigFloat}(:minimize, real(tr(E12 * (s1 + 2 * s2) + E21 * (s2 + 2 * s1))), [E12 ⪰ 0, E21 ⪰ 0, E12 + E21 == Diagonal(ones(2)) ])
 
-    solve!(p, SDPA_GMP.Optimizer(silent = true, presolve = true, variant = :dd))
-    @test p.optval ≈ (6 - sqrt(big"2.0"))/4 atol=1e-10
+    solve!(p, SDPA_GMP.Optimizer(silent = false, presolve = true, variant = :dd))
+    @test p.optval ≈ (6 - sqrt(big"2.0"))/4 atol=1e-15
 
     solve!(p, SDPA_GMP.Optimizer(silent = true, presolve = true, variant = :qd))
-    @test p.optval ≈ (6 - sqrt(big"2.0"))/4 atol=1e-10
+    @test p.optval ≈ (6 - sqrt(big"2.0"))/4 atol=1e-15
 
     solve!(p, SDPA_GMP.Optimizer(silent = true, presolve = true, variant = :gmp))
     @test p.optval ≈ (6 - sqrt(big"2.0"))/4 atol=1e-30
