2a6e11ece790dd594fad3516b453d6cb5918cb48
Eric <5846501+ericphanson@users.noreply.github.com>
2019-09-10 07:33:22 -0400

    Remove exp, add logging for timeouts + info

--- test/Convex.jl
+++ test/Convex.jl
@@ -1,11 +1,17 @@
 using Convex: Convex
-using Convex.ProblemDepot: run_tests
+using Convex.ProblemDepot: run_tests, foreach_problem
 
 
 @testset "Convex tests with variant $var" begin
-    run_tests(; exclude=[   r"mip",
-                            r"socp"
-                        ]) do p
-        solve!(p, SDPAFamily.Optimizer{Float64}(presolve = true, silent = true, variant = var))
+    foreach_problem(;exclude=[  r"mip",
+                                r"exp",
+                                r"benchmark"
+                            ]) do name, problem_func
+        @testset "$name" begin
+            problem_func(Val(true), 1e-3, 0.0, Float64) do problem
+                @info "Testing" name var
+                solve!(p, SDPAFamily.Optimizer{Float64}(presolve = true, silent = true, variant = var))
+            end
+        end
     end
 end
