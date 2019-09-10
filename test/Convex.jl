f809678d298d0f212a77515a438d2555ea059f04
Eric <5846501+ericphanson@users.noreply.github.com>
2019-09-10 08:15:54 -0400

    Remove `lp_min_atom` fow now

--- test/Convex.jl
+++ test/Convex.jl
@@ -5,7 +5,8 @@ using Convex.ProblemDepot: run_tests, foreach_problem
 @testset "Convex tests with variant $var" begin
     foreach_problem(;exclude=[  r"mip",
                                 r"exp",
-                                r"benchmark"
+                                r"benchmark",
+                                r"lp_min_atom", # hang
                             ]) do name, problem_func
         @testset "$name" begin
             problem_func(Val(true), 1e-3, 0.0, Float64) do p
