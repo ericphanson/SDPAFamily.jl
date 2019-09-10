2aa0699f3ba9605e0a92a57a1b4985a64b4298de
JiazhengZhu <zhujiazheng98@hotmail.com>
2019-09-10 17:24:42 +0100

    excluding more tests

--- test/Convex.jl
+++ test/Convex.jl
@@ -6,17 +6,23 @@ common_excludes = [  r"mip",
                     r"benchmark",
                     r"lp_min_atom", # hangs
                     r"lp_max_atom", # hangs
+                    r"sdp_Complex_Semidefinite_constraint", # too large
                     ]
 excludes_dict = Dict(:gmp => vcat(common_excludes,Regex[
-                        r"affine_Partial_transpose" # slow
+                        r"affine_Partial_transpose", # slow
+                        r"affine_Diagonal_atom" # underflows
                     ]),
                     :dd =>  vcat(common_excludes,Regex[
-                        r"affine_Partial_transpose" # slow
+                        r"affine_Partial_transpose", # slow
+                        r"affine_Diagonal_atom" # underflows
                     ]),
                     :qd =>  vcat(common_excludes,Regex[
-                        r"affine_Partial_transpose" # slow
+                        r"affine_Partial_transpose", # slow
+                        r"affine_Diagonal_atom" # underflows
                     ]),
-                    :plain => vcat(common_excludes, Regex[]),)
+                    :plain => vcat(common_excludes,Regex[
+                        r"affine_Partial_transpose" # slow
+                    ]),)
 
 @testset "Convex tests with variant $var" begin
     foreach_problem(;exclude=excludes_dict[var]) do name, problem_func
