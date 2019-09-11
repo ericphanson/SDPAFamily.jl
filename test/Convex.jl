d74aa2b3cc6770342ed616e076185d75275a83f9
JiazhengZhu <zhujiazheng98@hotmail.com>
2019-09-11 22:18:06 +0100

    update tests

--- test/Convex.jl
+++ test/Convex.jl
@@ -1,35 +1,33 @@
 using Convex: Convex
 using Convex.ProblemDepot: run_tests, foreach_problem
 
-const common_excludes = [   
+const common_excludes = [
                             r"mip", # doesn't support MIP
                             r"exp", # doesn't support exponential cone
                             r"benchmark", # don't include benchmark-only problems
-                            r"lp_min_atom", # hangs
-                            r"lp_max_atom", # hangs
                             r"sdp_Complex_Semidefinite_constraint", # too large
-                            r"affine_Partial_transpose", # slow
                         ]
 
 const excludes_dict = Dict(:sdpa_gmp => vcat(common_excludes,
-                        Regex[
-                            # r"affine_Diagonal_atom" # underflows
+                        Regex[r"affine_Partial_transpose", # underflows
+
                         ]),
                     :sdpa_dd =>  vcat(common_excludes,
-                        Regex[
-                            r"affine_Diagonal_atom" # underflows
+                        Regex[r"affine_Partial_transpose", # underflows
+
                         ]),
                     :sdpa_qd =>  vcat(common_excludes,
                         Regex[
+                            r"affine_Partial_transpose", # underflows
                             r"affine_Diagonal_atom" # underflows
                         ]),
                     :sdpa => vcat(common_excludes,
                         Regex[
-                            r"affine_Diagonal_atom", # underflows
-                            r"lp_dotsort_atom" # imprecise
+                            r"lp_dotsort_atom", # imprecise
+                            r"lp_pos_atom" # imprecise
                         ]))
 
-const no_presolve_problems = ["affine_Diagonal_atom"]
+const no_presolve_problems = ["affine_Diagonal_atom", "lp_min_atom", "lp_max_atom"]
 
 @testset "Convex tests with variant $var" begin
     foreach_problem(;exclude=excludes_dict[var]) do name, problem_func
