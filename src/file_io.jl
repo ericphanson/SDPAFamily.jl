1be3bd81a0cf366915afd62fce17abe1bfe95402
JiazhengZhu <zhujiazheng98@hotmail.com>
2019-09-13 00:34:36 +0100

    Revert "Revert "underflow warning""
    This reverts commit f32c2a6e90d4d9dbd446f4c052fe49e05e415e86.

--- src/file_io.jl
+++ src/file_io.jl
@@ -87,6 +87,9 @@ function read_results!(
                 line = getnextline(io)
             end
         end
+    if norm(optimizer.b, Inf) < eps(norm(xMatvec, Inf)) || norm(optimizer.b, Inf) < eps(norm(yMatvec, Inf))
+        @warn "Potential underflow detected. Check the results and use `BigFloat` entries if necessary."
+    end
     xVecstring = remove_brackets!(xVecstring)
     xVecstring = split(xVecstring, ",")
     xVec = parse.(T, xVecstring)
