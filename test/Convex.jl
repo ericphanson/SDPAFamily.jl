b31d61cb7613f3e97cca25e9b15c129622250afb
Eric <5846501+ericphanson@users.noreply.github.com>
2019-09-12 11:20:50 -0400

    Try testing with BigFloats

--- test/Convex.jl
+++ test/Convex.jl
@@ -1,41 +1,56 @@
+using Test, SDPAFamily
 using Convex: Convex
 using Convex.ProblemDepot: run_tests, foreach_problem
+using GenericLinearAlgebra
 
-const common_excludes = [
-                            r"mip", # doesn't support MIP
-                            r"exp", # doesn't support exponential cone
+# Problems that cannot be handled by any variant with any numeric type
+const common_excludes = Regex[
+                            r"mip", # SDPA solvers don't support mixed integer programming
+                            r"exp", # SDPA solvers don't support the exponential cone (no bridge yet?)
                             r"benchmark", # don't include benchmark-only problems
                             r"sdp_Complex_Semidefinite_constraint", # too large
                         ]
 
-const excludes_dict = Dict(:sdpa_gmp => vcat(common_excludes,
-                        Regex[r"affine_Partial_transpose", # underflows
+# Problems that cannot be handled due to issues with a certain numeric type,
+# independent of variant.
+const type_excludes = Dict( Float64 => Regex[],
+                            BigFloat => Regex[
+                                r"sdp_lambda_max_atom", # GenericLinearAlgebra#47
+                                r"socp", # MathOptInterface.jl#876
+                        ])
 
-                        ]),
-                    :sdpa_dd =>  vcat(common_excludes,
-                        Regex[r"affine_Partial_transpose", # underflows
-
-                        ]),
-                    :sdpa_qd =>  vcat(common_excludes,
-                        Regex[
+# Problems that cannot be handled with a specific combination of variant and numeric type
+# (often due to things like underflow).
+const variant_excludes = Dict(
+                    (:sdpa_gmp, Float64) => Regex[
+                            r"affine_Partial_transpose", # underflows
+                        ],
+                    (:sdpa_dd, Float64) =>  Regex[
+                            r"affine_Partial_transpose", # underflows
+                        ],
+                    (:sdpa_qd, Float64) =>  Regex[
                             r"affine_Partial_transpose", # underflows
                             r"affine_Diagonal_atom" # underflows
-                        ]),
-                    :sdpa => vcat(common_excludes,
-                        Regex[
+                        ],
+                    (:sdpa, Float64) => Regex[
                             r"lp_dotsort_atom", # imprecise
                             r"lp_pos_atom" # imprecise
-                        ]))
+                        ],
+                    (:sdpa, BigFloat) => Regex[
+                            r"lp_dotsort_atom", # imprecise
+                            r"lp_pos_atom" # imprecise
+                        ])
 
 const no_presolve_problems = ["affine_Partial_transpose", "lp_min_atom", "lp_max_atom"]
 
-@testset "Convex tests with variant $var" begin
-    foreach_problem(;exclude=excludes_dict[var]) do name, problem_func
+@testset "Convex tests with variant $var and type $T" for T in (Float64, BigFloat)
+    excludes = vcat(common_excludes, get(variant_excludes, (var, T), Regex[]), type_excludes[T])
+    foreach_problem(;exclude = excludes) do name, problem_func
         @testset "$name" begin
-            problem_func(Val(true), 1e-3, 0.0, Float64) do p
-                @info "`solve!` called" name var
+            problem_func(Val(true), 1e-3, 0.0, T) do p
+                @info "`solve!` called" name var T
                 presolve = !(name ∈ no_presolve_problems)
-                time = @elapsed solve!(p, SDPAFamily.Optimizer{Float64}(presolve = presolve, silent = true, variant = var))
+                time = @elapsed Convex.solve!(p, SDPAFamily.Optimizer{T}(presolve = presolve, silent = true, variant = var))
                 @info "Finished `solve!`" time
             end
         end
